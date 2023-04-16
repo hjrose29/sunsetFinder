a7dde44f967d5f22e780d52db99ccda5a8bbdbc0
Tristan Allen <tristanallen@Tristans-MacBook-Air.local>
2023-04-16 15:52:31 -0400

    searchByDate Function

---
+++ SearchByDate.py
@@ -0,0 +1,37 @@
+import os
+from datetime import datetime
+
+filepath = "C:/Users/trist/PycharmProjects/SunsetFinder/sunset"
+
+
+# search for the input date, or find the closest date
+def search(date):
+    closest_date = None
+    closest_filename = None
+    matches = []
+    input_date_object = datetime.strptime(formatted_date, '%Y%m%d')
+    for filename in os.listdir(filepath):
+        formatted_file = filename.split('-')[0]
+        file_date_object = datetime.strptime(formatted_file, '%Y%m%d')
+        if date == formatted_file:
+            matches.append(filename)
+        elif closest_date is None or abs(file_date_object - input_date_object) < abs(closest_date - input_date_object):
+            closest_date = file_date_object
+            closest_filename = filename
+    if matches:
+        return matches
+    elif closest_date is not None:
+        return closest_filename
+    else:
+        return None
+
+
+# take input and format to YYYYMMDD
+input_date = input("Enter a date... ")
+month, day, year = input_date.split('/')
+formatted_date = year + month + day
+
+if search(formatted_date) is not None:
+    print(search(formatted_date))
+else:
+    print("No file found for: " + input_date)
