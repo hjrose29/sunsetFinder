0103b6fb19bf4cc51bcc978bbaaa9db7d413d5c8
Henry <henryjrose2@gmail.com>
2023-04-29 21:40:21 -0400

    Functional Site

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from datetime import date
+import shutil
 from flask import Flask, render_template,request, redirect
 from jsonLoader import *
 from forms import dateForm
@@ -7,11 +8,17 @@ from forms import dateForm
 app = Flask(__name__,template_folder="templates")
 
 @app.route("/")
-def hello():
+def index():
     return render_template('index.html')
 
 @app.route("/archive.html",methods=['GET', 'POST'])
 def archive():
+    toDisplay = "/Users/henry/Documents/sunsetFinder/toDisplay/"
+    for i in os.listdir(toDisplay):
+        encoded, date = toDisplayToJson(toDisplay, i)
+        update_json(encoded, date)
+        shutil.move(toDisplay + i, "/Users/henry/Documents/sunsetFinder/app/Archive/" + i)
+
     images = getImages("archive.json")
     search = dateForm(request.form)
     if request.method == 'POST':
@@ -19,6 +26,11 @@ def archive():
         return render_template('archive.html', c0=images[0], c1=images[1],c2=images[2],c3=images[3])
     #dates = getDates("app/templates/archive.json")
     return render_template('archive.html', c0=images[0], c1=images[1],c2=images[2],c3=images[3])
+
+@app.route("/aboutUs.html")
+def aboutUs():
+    return render_template('aboutUs.html')
+
 
 if __name__ == "__main__":
     app.run()
